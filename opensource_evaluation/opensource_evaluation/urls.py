cd9294a1f5fa668a5f4f96302de8378d102dfa83
maruiNia <maruinano@gmail.com>
2025-07-05 15:18:43 +0900

    modify managers app testing managers app is successful

--- opensource_evaluation/opensource_evaluation/urls.py
+++ opensource_evaluation/opensource_evaluation/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("manager/", admin.site.urls),
+    path("managers/", include("apps.managers.urls")),
 ]
